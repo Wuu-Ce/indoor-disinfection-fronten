0ab09813feb3b1fd99b7a6a3c0c04a4a2599e7af
qdlaoxu <442413729@qq.com>
2017-04-26 19:25:21 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App';
 
-// APP样式
+
+// APP style
 import './assets/css/bootstrap.css';
 import './assets/css/app.css';
 import './assets/css/font.css';
 import './assets/css/font-awesome.min.css';
 import './assets/css/simple-line-icons.css';
 
+
+Vue.use(Vuex);
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // store,
   template: '<App/>',
   components: { App },
 });
